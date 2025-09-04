abb2a4bcdea9d4cfcd7de4717e86e0910819b756
Kenan Iusubovi <kenan.iusubovi@gmail.com>
2025-09-04 04:59:04 +0400

    finish class TrainMainTenanceRecord

--- src/train/TrainMaintenanceRecord.java
+++ src/train/TrainMaintenanceRecord.java
@@ -1,8 +1,12 @@
 package train;
 
+import people.worker.Worker;
+
 import java.time.LocalDateTime;
 
 public class TrainMaintenanceRecord {
+
+    private static long idCounter = 0;
 
     private long id;
 
@@ -14,5 +18,63 @@ public class TrainMaintenanceRecord {
 
     private double cost;
 
-    private Mechanic mechanic;
+    private Worker mechanic;
+
+    public TrainMaintenanceRecord(Train train, LocalDateTime maintenanceDateTime,
+                                  String description, double cost, Worker mechanic) {
+        this.id = ++idCounter;
+        this.train = train;
+        this.maintenanceDateTime = maintenanceDateTime;
+        this.description = description;
+        this.cost = cost;
+        this.mechanic = mechanic;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Train getTrain() {
+        return train;
+    }
+
+    public void setTrain(Train train) {
+        this.train = train;
+    }
+
+    public LocalDateTime getMaintenanceDateTime() {
+        return maintenanceDateTime;
+    }
+
+    public void setMaintenanceDateTime(LocalDateTime maintenanceDateTime) {
+        this.maintenanceDateTime = maintenanceDateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public Worker getMechanic() {
+        return mechanic;
+    }
+
+    public void setMechanic(Worker mechanic) {
+        this.mechanic = mechanic;
+    }
 }
