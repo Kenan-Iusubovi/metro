609cc2d03fe7a9368c8833dc68aa80ccea0fa9f8
Kenan Iusubovi <kenan.iusubovi@gmail.com>
2025-09-04 04:55:13 +0400

    finish class Ticket

--- src/ticket/Ticket.java
+++ src/ticket/Ticket.java
@@ -21,4 +21,73 @@ public class Ticket {
     private TicketStatusE ticketStatus;
 
     private Passenger passenger;
+
+    public Ticket(UUID ticketNumber, Route route, double price,
+                  LocalDateTime validFrom, LocalDateTime validUntil,
+                  TicketStatusE ticketStatus, Passenger passenger) {
+        this.ticketNumber = ticketNumber;
+        this.route = route;
+        this.price = price;
+        this.validFrom = validFrom;
+        this.validUntil = validUntil;
+        this.ticketStatus = ticketStatus;
+        this.passenger = passenger;
+    }
+
+
+    public UUID getTicketNumber() {
+        return ticketNumber;
+    }
+
+    public void setTicketNumber(UUID ticketNumber) {
+        this.ticketNumber = ticketNumber;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public void setRoute(Route route) {
+        this.route = route;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public LocalDateTime getValidFrom() {
+        return validFrom;
+    }
+
+    public void setValidFrom(LocalDateTime validFrom) {
+        this.validFrom = validFrom;
+    }
+
+    public LocalDateTime getValidUntil() {
+        return validUntil;
+    }
+
+    public void setValidUntil(LocalDateTime validUntil) {
+        this.validUntil = validUntil;
+    }
+
+    public TicketStatusE getTicketStatus() {
+        return ticketStatus;
+    }
+
+    public void setTicketStatus(TicketStatusE ticketStatus) {
+        this.ticketStatus = ticketStatus;
+    }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(Passenger passenger) {
+        this.passenger = passenger;
+    }
 }
