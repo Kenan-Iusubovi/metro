2ce430174bb43c7bea679623d06bee0351d1a113
Kenan Iusubovi <kenan.iusubovi@gmail.com>
2025-09-04 04:54:09 +0400

    finish class Turnstile

--- src/station/Turnstile.java
+++ src/station/Turnstile.java
@@ -2,9 +2,18 @@ package station;
 
 public class Turnstile {
 
+    private static long idCounter = 0;
+
     private long id;
 
     private boolean isActive;
 
     private boolean isLocked;
+
+    public Turnstile() {
+
+        this.id = ++idCounter;
+        this.isActive = true;
+        this.isLocked = true;
+    }
 }
