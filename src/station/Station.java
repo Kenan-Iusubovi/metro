4f5071a5dc259beddcdf7ae6c0266a6f152ebd4b
Kenan Iusubovi <kenan.iusubovi@gmail.com>
2025-09-04 04:52:38 +0400

    finish class Station

--- src/station/Station.java
+++ src/station/Station.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public class Station {
 
+    private static long idCounter = 0;
+
     private long id;
 
     private long code;
@@ -11,4 +13,44 @@ public class Station {
     private String name;
 
     private List<Turnstile> turnstiles;
+
+    public Station(long code, String name, List<Turnstile> turnstiles) {
+
+        this.id = ++idCounter;
+        this.code = code;
+        this.name = name;
+        this.turnstiles = turnstiles;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getCode() {
+        return code;
+    }
+
+    public void setCode(long code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Turnstile> getTurnstiles() {
+        return turnstiles;
+    }
+
+    public void setTurnstiles(List<Turnstile> turnstiles) {
+        this.turnstiles = turnstiles;
+    }
 }
