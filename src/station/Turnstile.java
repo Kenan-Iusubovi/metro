30402eb53ed3a8310e4c1b46fff55f68ef878f44
Kenan Iusubovi <kenan.iusubovi@gmail.com>
2025-09-05 17:10:48 +0400

    refactor Turnstile

--- src/station/Turnstile.java
+++ src/station/Turnstile.java
@@ -5,17 +5,12 @@ import ticket.Ticket;
 public class Turnstile {
 
     private static long idCounter = 0;
-
-    private long id;
-
+    private Long id;
     private String code;
-
     private boolean active;
-
     private boolean closed;
 
     public Turnstile(String code, boolean active, boolean closed) {
-
         this.id = ++idCounter;
         setCode(code);
         setActive(active);
@@ -23,53 +18,45 @@ public class Turnstile {
     }
 
     public long getId() {
-
         return id;
     }
 
     public String getCode() {
-
         return code;
     }
 
     public void setCode(String code) {
-
-        if (code == null || code.isBlank()) throw new IllegalArgumentException("code");
+        if (code == null || code.isBlank()){
+            throw new IllegalArgumentException("Code can't be null or negative.");
+        }
         this.code = code;
     }
 
     public boolean isActive() {
-
         return active;
     }
 
     public void activate() {
-
         this.active = true;
     }
 
     public void deactivate() {
-
         this.active = false;
     }
 
     public boolean isClosed() {
-
         return closed;
     }
 
     public void open() {
-
         this.closed = false;
     }
 
     public void close() {
-
         this.closed = true;
     }
 
     public boolean pass(Ticket ticket) {
-
         if (!active) {
             System.out.println("Turnstile " + code + " is deactivated.");
             return false;
@@ -87,17 +74,15 @@ public class Turnstile {
             return false;
         }
         ticket.useForEntry();
-        System.out.println("Ticket " + ticket.getId() + " accepted at " + code);
+        System.out.println("Ticket " + ticket.getId() + " accepted at turnstile" + code);
         return true;
     }
 
     public void setActive(boolean active) {
-
         this.active = active;
     }
 
     public void setClosed(boolean closed) {
-
         this.closed = closed;
     }
 }
