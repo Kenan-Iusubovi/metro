954c6d6cc020ed0340b97ead2a3185e22cb48e63
Kenan Iusubovi <kenan.iusubovi@gmail.com>
2025-10-09 22:27:17 +0400

    replace sout & error messages  with logger in Human

--- src/main/java/com/solvd/metro/domain/people/Human.java
+++ src/main/java/com/solvd/metro/domain/people/Human.java
@@ -1,8 +1,13 @@
 package com.solvd.metro.domain.people;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.time.LocalDate;
 
 public abstract class Human {
+
+    private static final Logger logger = LogManager.getLogger(Human.class);
 
     private String firstname;
     private String surname;
@@ -26,6 +31,7 @@ public abstract class Human {
 
     public void setFirstname(String firstname) {
         if (firstname == null || firstname.isEmpty()) {
+            logger.error("Firstname can't be null or empty.");
             throw new IllegalArgumentException("Firstname can't be null or empty.");
         }
         this.firstname = firstname;
@@ -37,6 +43,7 @@ public abstract class Human {
 
     public void setSurname(String surname) {
         if (surname == null || surname.isEmpty()) {
+            logger.error("Surname can't be null or empty.");
             throw new IllegalArgumentException("Surname can't be null or empty.");
         }
         this.surname = surname;
@@ -48,6 +55,7 @@ public abstract class Human {
 
     public void setDateOfBirth(LocalDate dateOfBirth) {
         if (dateOfBirth == null) {
+            logger.error("Date of birth can't be null.");
             throw new IllegalArgumentException("Date of birth can't be null.");
         }
         this.dateOfBirth = dateOfBirth;
